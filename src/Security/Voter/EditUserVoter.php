12aeb5018205f94b1dc420633634facdcefbf1ad
JeanD34 <jean.webdev@gmail.com>
2019-08-03 14:40:10 +0200

    Get the array of roles with parameter

--- src/Security/Voter/EditUserVoter.php
+++ src/Security/Voter/EditUserVoter.php
@@ -2,13 +2,20 @@
 
 namespace App\Security\Voter;
 
-use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Psr\Container\ContainerInterface;
 
 class EditUserVoter extends Voter
 {
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     protected function supports($attribute, $subject)
     {
         return in_array($attribute, ['EDIT'])
@@ -23,12 +30,8 @@ class EditUserVoter extends Voter
             return false;
         }
 
-        // Path to security.yaml
-        $path = $_SERVER['DOCUMENT_ROOT'] . '/../config/packages/security.yaml';
-
-        // Parse file and get the array of roles
-        $value = Yaml::parseFile($path);
-        $roles = $value['security']['role_hierarchy'];
+        // Get the array of roles
+        $roles =  $this->container->getParameter('security.role_hierarchy.roles');
 
         // Get an array of value
         $value = array_values($roles);
@@ -41,6 +44,7 @@ class EditUserVoter extends Voter
         // array_flip to have keys becoming values (Values become numbers so ROLE have numerical value and can be compare)
         // Example : "ROLE_USER" => 0, "ROLE_ADMIN" => 1
         $roles = array_flip(array_unique(array_merge($value, $key)));
+
 
         switch ($attribute) {
             case 'EDIT':
